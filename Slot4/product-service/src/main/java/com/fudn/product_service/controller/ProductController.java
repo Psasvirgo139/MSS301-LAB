b00806730b1eb16136fb9a1fdebe6976c158ad77
Khoi <nguyenngockhoi273@gmail.com>
2026-09-25 15:46:40 +0700

    feat(controller): add updateProduct endpoint

--- Slot4/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
+++ Slot4/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
@@ -50,8 +50,11 @@ public class ProductController {
     //   public ProductResponse updateProduct(
     //          @PathVariable String id,
     //          @RequestBody ProductRequest productRequest) { ... }
-    // ==========================================================
-    // TODO: viết endpoint update tại đây
+    @PutMapping("/{id}")
+    public ProductResponse updateProduct(@PathVariable String id,
+                                          @RequestBody ProductRequest productRequest) {
+        return productService.updateProduct(id, productRequest);
+    }
 
 
     // ==========================================================
